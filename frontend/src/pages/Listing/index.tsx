d0394369bb4958e6e403f64ae65e69e74abd0a1c
Rafael Gonzaga <ralozaga@gmail.com>
2022-01-14 15:50:45 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,20 +1,33 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    // Forma errada. Pequeno teste de requisição
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-        .then(response => {
-            console.log(response.data);
-            
-        });
+    const [pageNumber, setPageNumber] = useState(0); 
+
+    useEffect(()=>{
+        // Forma errada. Pequeno teste de requisição
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                console.log(data);
+                
+                setPageNumber(data.number);
+                
+            });
+
+    }, []);
+
+
 
     return (
 
         <>
+        <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
                 <div className="row">
