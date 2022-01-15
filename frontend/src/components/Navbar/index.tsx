63e93f1ff440ce508c504bc9f46e0718781da953
Rafael Gonzaga <ralozaga@gmail.com>
2022-01-15 12:38:02 -0300

    Link corretion

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
                     <a href="https://github.com/rafagonzaga">
                         <div className='dsmovie-contact-container'>
                             <GithubIcon />
-                            <p className='dsmovie-contact-link'>/devsuperior</p>
+                            <p className='dsmovie-contact-link'>/rafagonzaga</p>
                         </div>
 
                     </a>
